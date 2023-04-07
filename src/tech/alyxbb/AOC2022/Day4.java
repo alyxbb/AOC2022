9377473127fb0edf6c8750a185f1996ed89a9d8f
alyx <git@alyxbb.tech>
2023-04-07 19:01:38 +0100

    day 4

---
+++ src/tech/alyxbb/AOC2022/Day4.java
@@ -0,0 +1,47 @@
+package tech.alyxbb.AOC2022;
+
+import java.util.Arrays;
+
+public class Day4 extends Day {
+    public Day4(String inp) {
+        super(inp);
+    }
+
+    int part1() {
+        String[] elfPairs = input.split("\n");
+        int count = 0;
+        for (String elfPair : elfPairs) {
+            String[] pairArr = elfPair.split(",");
+            int[] firstElf = Arrays.stream(pairArr[0].split("-")).mapToInt(Integer::parseInt).toArray();
+            int[] secondElf = Arrays.stream(pairArr[1].split("-")).mapToInt(Integer::parseInt).toArray();
+            if (firstElf[0] <= secondElf[0] && secondElf[1] <= firstElf[1]) {
+                count++;
+            } else if (secondElf[0] <= firstElf[0] && firstElf[1] <= secondElf[1]) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    int part2() {
+        String[] elfPairs = input.split("\n");
+        int count = 0;
+        for (String elfPair : elfPairs) {
+            String[] pairArr = elfPair.split(",");
+            int[] firstElf = Arrays.stream(pairArr[0].split("-")).mapToInt(Integer::parseInt).toArray();
+            int[] secondElf = Arrays.stream(pairArr[1].split("-")).mapToInt(Integer::parseInt).toArray();
+            if (firstElf[0] <= secondElf[0] && secondElf[0] <= firstElf[1]) {
+                count++;
+            } else if (firstElf[0] <= secondElf[1] && secondElf[1] <= firstElf[1]) {
+                count++;
+            } else if (secondElf[0] <= firstElf[0] && firstElf[0] <= secondElf[1]){
+                count++;
+            } else if (secondElf[0] <= firstElf[1] && firstElf[1] <= secondElf[1]){
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
