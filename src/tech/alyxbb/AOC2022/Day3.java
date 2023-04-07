d17e3b2b24514fd1ef4739475c70dc85e86e5236
alyx <git@alyxbb.tech>
2023-04-07 17:33:01 +0100

    day 3

---
+++ src/tech/alyxbb/AOC2022/Day3.java
@@ -0,0 +1,69 @@
+package tech.alyxbb.AOC2022;
+
+public class Day3 extends Day {
+
+    public Day3(String inp) {
+        super(inp);
+    }
+
+    int part1() {
+        String[] rucksacks = input.split("\n");
+        int tot = 0;
+        for (String rucksack : rucksacks) {
+            String[] rucksackParts = {rucksack.substring(0, rucksack.length() / 2), rucksack.substring(rucksack.length() / 2)};
+            char duplicate = findMatching(rucksackParts);
+            tot += getPriority(duplicate);
+
+        }
+        return tot;
+    }
+
+    private static int getPriority(char duplicate) {
+        int priority;
+        if (duplicate >= 97) {
+            priority = duplicate - 96;
+        } else {
+            priority = duplicate - 38;
+        }
+        return priority;
+    }
+
+    private static char findMatching(String[] rucksackParts) {
+        for (char c : rucksackParts[0].toCharArray()) {
+            for (char d : rucksackParts[1].toCharArray()) {
+                if (c == d) {
+                    return c;
+                }
+            }
+        }
+        throw new RuntimeException("Invalid input");
+    }
+
+    int part2() {
+        String[] rucksacks = input.split("\n");
+        int tot = 0;
+        for (int rucksack = 0; rucksack < rucksacks.length; rucksack += 3) {
+
+            char duplicate = findMatchingThree(rucksacks[rucksack], rucksacks[rucksack + 1], rucksacks[rucksack + 2]);
+            tot += getPriority(duplicate);
+
+        }
+        return tot;
+    }
+
+    private char findMatchingThree(String rucksack1, String rucksack2, String rucksack3) {
+        for (char c : rucksack1.toCharArray()) {
+            for (char d : rucksack2.toCharArray()) {
+                for (char e : rucksack3.toCharArray()) {
+                    if (c == d && c == e) {
+                        return c;
+                    }
+                }
+
+            }
+        }
+        throw new RuntimeException("Invalid input");
+
+    }
+
+}
