b8aa13bb38983ec0bd0f454ddb0392089171aaed
alyx <git@alyxbb.tech>
2023-04-07 16:34:41 +0100

    day 2

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Advent Of Code 2022
+
+My solutions to the 2022 advent of code;
+
+Written in Java as I'm trying to learn Java;
--- src/tech/alyxbb/AOC2022/Day2.java
+++ src/tech/alyxbb/AOC2022/Day2.java
@@ -3,17 +3,59 @@ package tech.alyxbb.AOC2022;
 public class Day2 extends Day {
 
 
-    public Day2(final String inp) {
+    public Day2(String inp) {
         super(inp);
     }
 
     int part1() {
-        System.out.println("I am from day 2");
-        return 0;
+        String[] matches = input.split("\n");
+        int score = 0;
+        for (String match : matches) {
+            String[] players = match.split(" ");
+            int p1 = 0;
+            int p2 = 0;
+            switch (players[0]) {
+                case "A" -> p1 = -1;
+                case "B" -> p1 = 0;
+                case "C" -> p1 = 1;
+            }
+            switch (players[1]) {
+                case "X" -> p2 = -1;
+                case "Y" -> p2 = 0;
+                case "Z" -> p2 = 1;
+            }
+            score += p2 + 2;
+            if (p1 == p2) {
+                score += 3;
+            } else {
+                int result = p1 - p2;
+                if (2 == result || -1 == result) {
+                    score += 6;
+                }
+            }
+        }
+        return score;
     }
 
     int part2() {
-        System.out.println("I am also from day 2");
-        return 0;
+
+        String[] matches = input.split("\n");
+        int score = 0;
+        for (String match : matches) {
+            String[] players = match.split(" ");
+            int result = 0;
+            switch (players[1]) {
+                case "X" -> result = -1;
+                case "Y" -> result = 0;
+                case "Z" -> result = 1;
+            }
+            score += 3 * (result + 1);
+            switch (match) {//there is probably a better way to do this, but I couldnt find one
+                case "A X", "C Y", "B Z" -> score += 3;
+                case "B X", "A Y", "C Z" -> score += 1;
+                case "C X", "B Y", "A Z" -> score += 2;
+            }
+        }
+        return score;
     }
 }
